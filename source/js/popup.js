6875e9e6bd0ee02d5a7b2f93775a01cbd8cef918
Makhova <makhova.maria@gmail.com>
2018-10-31 13:39:06 +0300

    Исправлены отступы

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,13 +1,11 @@
 var formButton = document.querySelector(".button--user-review");
-var popupSuccess = document.querySelector(".popup--success");
-var closePopup = document.querySelector(".button--popup-success");
+var popupSuccess = document.querySelector(".popup--failure");
+var closePopup = document.querySelector(".button--popup-failure");
 
-formButton.addEventListener("click", function (evt) {
-
-
+formButton.addEventListener("click", function () {
   popupSuccess.classList.remove("popup--nojs");
 });
 
-closePopup.addEventListener("click", function (evt) {
+closePopup.addEventListener("click", function () {
   popupSuccess.classList.add("popup--nojs");
 });
